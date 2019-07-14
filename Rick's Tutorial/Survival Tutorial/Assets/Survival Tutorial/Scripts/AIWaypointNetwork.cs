56309f4ea93bed3ba14aed7581c3d8890a5ca580
Rick <rs90th@gmail.com>
2019-07-13 20:26:26 -0400

    Getting back to it

--- Rick's Tutorial/Survival Tutorial/Assets/Survival Tutorial/Scripts/AIWaypointNetwork.cs
+++ Rick's Tutorial/Survival Tutorial/Assets/Survival Tutorial/Scripts/AIWaypointNetwork.cs
@@ -1,8 +1,27 @@
-﻿using System.Collections;
+﻿using UnityEngine;
+using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 
+// Display Mode that the Custom Inspector of an AIWaypointNetwork
+// component can be in
+public enum PathDisplayMode { None, Connections, Paths }
+
+// -------------------------------------------------------------------
+// CLASS	:	AIWaypointNetwork
+// DESC		:	Contains a list of waypoints. Each waypoint is a 
+//				reference to a transform. Also contains settings
+//				for the Custom Inspector
+// ------------------------------------------------------------------
 public class AIWaypointNetwork : MonoBehaviour
 {
-    public List<Transform> waypoints = new List<Transform>();
+    [HideInInspector]
+    public PathDisplayMode DisplayMode = PathDisplayMode.Connections;   // Current Display Mode
+    [HideInInspector]
+    public int UIStart = 0;                                         // Start wayopoint index for Paths mode
+    [HideInInspector]
+    public int UIEnd = 0;                                           // End waypoint index for Paths mode
+
+    // List of Transform references
+    public List<Transform> Waypoints = new List<Transform>();
+
 }
