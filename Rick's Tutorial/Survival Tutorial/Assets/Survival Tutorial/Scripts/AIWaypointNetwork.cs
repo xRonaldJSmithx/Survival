b40723cd089302d4ac38749de99553b8196fe50e
Rick <rs90th@gmail.com>
2019-07-17 22:11:50 -0400

    Used Rons files to fix an error with my models

--- Rick's Tutorial/Survival Tutorial/Assets/Survival Tutorial/Scripts/AIWaypointNetwork.cs
+++ Rick's Tutorial/Survival Tutorial/Assets/Survival Tutorial/Scripts/AIWaypointNetwork.cs
@@ -2,26 +2,16 @@
 using System.Collections;
 using System.Collections.Generic;
 
-// Display Mode that the Custom Inspector of an AIWaypointNetwork
-// component can be in
 public enum PathDisplayMode { None, Connections, Paths }
-
-// -------------------------------------------------------------------
-// CLASS	:	AIWaypointNetwork
-// DESC		:	Contains a list of waypoints. Each waypoint is a 
-//				reference to a transform. Also contains settings
-//				for the Custom Inspector
-// ------------------------------------------------------------------
 public class AIWaypointNetwork : MonoBehaviour
 {
     [HideInInspector]
-    public PathDisplayMode DisplayMode = PathDisplayMode.Connections;   // Current Display Mode
+    public PathDisplayMode DisplayMode = PathDisplayMode.Connections;
     [HideInInspector]
-    public int UIStart = 0;                                         // Start wayopoint index for Paths mode
+    public int UIStart = 0;
     [HideInInspector]
-    public int UIEnd = 0;                                           // End waypoint index for Paths mode
+    public int UIEnd   = 0;
 
-    // List of Transform references
     public List<Transform> Waypoints = new List<Transform>();
 
 }
